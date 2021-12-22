cb3691f09b55778adfc32a70ee82e718087da082
Mario Freire <mjfneto@gmail.com>
2021-12-22 18:47:33 -0300

    List todos

--- src/index.js
+++ src/index.js
@@ -30,7 +30,10 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { todos } = user;
+
+  return response.json(todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
